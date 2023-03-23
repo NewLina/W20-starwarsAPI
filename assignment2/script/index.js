cb2ecbeb6d52eacf6846fd9052775cde74971b10
Имя <prybak497@gmail.com>
2023-03-23 20:19:57 +0300

    .

---
+++ assignment2/script/index.js
@@ -0,0 +1,12 @@
+//const moment = require('moment');
+
+JSON =
+{
+		"date": "data"
+	}
+
+
+const containerDate=document.querySelector('.container__date');
+
+// let date = moment();
+// console.log(date);
